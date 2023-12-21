8f8e3c311ca2d413741a1462e07d39c7f80f1f68
Danny <ydh.postkasten@yahoo.com>
2023-12-21 16:50:41 +0100

    DHE rem Funktionen und Label eingefügt

---
+++ KW51/Do/v10_funktion_trennstrich/function_trennstrich.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+function f_Trennstrich(){
+
+    echo "<br>" ;
+                        for( $i=1 ; $i<=44 ; $i+= 1 )
+                            echo "=" ;
+    echo "<br>" ;
+}
+
+
---
+++ KW51/Do/v11_funktion_label/index.php
@@ -0,0 +1,37 @@
+<!-- header file index   -->
+    <?php
+
+        include_once 'label.inc.php' ;
+
+    ?>
+    <!DOCTYPE html>
+        <html lang="de">
+        <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Funktion Start</title>
+        </head>
+        <body>
+<!--        Body                -->
+<?php
+f_Bart();
+f_Label();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+</body>
+</html>
