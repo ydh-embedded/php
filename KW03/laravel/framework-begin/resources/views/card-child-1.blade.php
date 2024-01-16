064a64123678ce688f5fa9f66b1878910813c6f7
Danny <ydh.postkasten@yahoo.com>
2024-01-16 08:00:09 +0100

    DHE laravel config

---
+++ KW03/laravel/framework-begin/resources/views/card-child-1.blade.php
@@ -0,0 +1,7 @@
+@component('card')Inhalt der child-1
+
+    @slot('title')Title der child-1 @endslot
+
+    @slot('footer')Footer der child-1 @endslot
+    
+@endcomponent
---
+++ KW03/laravel/framework-begin/resources/views/card.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Komponenten des Rahmens</title>
+</head>
+<body>
+    <div class="card" style="width: 18rem">
+
+
+        <div class="card-header">
+            <h2>
+                Aus dem Views-Ordner
+                mit Cad-Header als H2
+            </h2>
+        </div>
+
+
+        <div class="card-body">
+            <h2>
+                Aus dem Views-Ordner
+                mit Card-Body als H2
+            </h2>
+            <h5 class="card-title">{{ $title }}</h5>
+        </div>
+
+
+
+    </div>
+</body>
+</html>
